e37b247d8770c705ddb384daadd253218eb03bea
Sougata01 <sougatadas01011999@gmail.com>
2024-04-08 18:19:09 +0530

    Managing Multiple States

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,20 +4,22 @@ import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
-const [title,setTitle] = useState(props.title)
-  function buttonClickHandler(event) {
-    setTitle("New Title")
+  const [title, setTitle] = useState(props.title);
+  
+  function buttonClickHandler() {
+    setTitle("New Title");
   }
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{title}</h2>
         <div className="expense-item__price">${props.price}</div>
-        <button onClick={buttonClickHandler}>Change Title</button>
       </div>
+      <button onClick={buttonClickHandler}>Change Title</button>
     </Card>
   );
-}
+};
 
 export default ExpenseItem;
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,22 +1,22 @@
-import "./Expenses.css"
 import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css";
 import Card from "../UI/Card";
 
 function Expenses(props) {
-    return (
-        <Card className="expenses">
-            {props.expenses.map((expense, index) => {
-                return (
-                    <ExpenseItem
-                        key={expense.id}
-                        date={expense.date}
-                        title={expense.title}
-                        price={expense.price}
-                    ></ExpenseItem>
-                );
-            })}
-        </Card>
-    )
+  return (
+    <Card className="expenses">
+      {props.expenses.map((expense) => {
+        return (
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            date={expense.date}
+            price={expense.price}
+          />
+        );
+      })}
+    </Card>
+  );
 }
 
 export default Expenses;
